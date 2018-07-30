0e0e9b2851606f42f41dc31d233433698ead0eb9
Makan Azarshahy <oro1919@gmail.com>
2018-07-30 16:48:34 -0700

    Added more stylings to Styled Input

--- app/components/HomePage/Input.js
+++ app/components/HomePage/Input.js
@@ -1,10 +1,14 @@
 import styled from 'styled-components';
 
 const Input = styled.input`
-  height: 30px;
+  height: 40px;
   font-size: 1.1rem;
   width: 100%;
   font-weight: bolder;
+  border: 1px solid blue;
+  border-radius: 20px;
+  padding-left: 15px;
+  line-height: 1.1rem;
 `;
 
 export default Input;
