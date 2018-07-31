5ff21404316710e133e2f48f25f64d5db27707eb
Makan Azarshahy <oro1919@gmail.com>
2018-07-30 19:39:50 -0700

    Created basic wrapper

---
+++ app/components/Wrapper/index.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+const Wrapper = styled.div`
+  padding: 0 25px;
+  display: flex;
+  flex-flow: column;
+  justify-content: center;
+  align-items: center;
+`;
+
+export default Wrapper;
